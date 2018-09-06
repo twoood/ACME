f1143f92c0ac4cba8899e32c08dedba524f41422
twoood <31707618+twoood@users.noreply.github.com>
2018-09-06 12:25:52 -0500

    Add files via upload
    Updating works for the item numbers

--- acme_server.php
+++ acme_server.php
@@ -1,3 +1,14 @@
 <?php
-echo 'Thank you for your order of:'. $_POST['Rabbits']. ' Rabbits ' .$_POST['LBaskets']. ' Large Easter Baskets ' .$_POST['SBaskets']. ' Small Easter Baskets ' .$_POST['Eggs'] . ' Easter Eggs.';
+	// $q_rabbit = $_POST['Rabbits'];
+	// $q_lbasket = $_POST['LBaskets'];
+	// $q_sbasket = $_POST['SBaskets'];
+	$h_order = $_REQUEST["order"];
+	// $group = $_REQUEST["group"];
+	$order = html_entity_decode($h_order);
+	$d_order = json_decode($order, TRUE);
+	// $d_group = json_decode($group, true);
+
+
+
+	print $d_order["Rabbits"];
 ?>
