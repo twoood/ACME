7f3e3fd1d3cfe9b7fc0d28a96179447289b94f47
twoood <31707618+twoood@users.noreply.github.com>
2018-09-06 14:11:35 -0500

    Update acme_server.php

--- acme_server.php
+++ acme_server.php
@@ -1,8 +1,12 @@
 <?php
+
 	$h_order = $_REQUEST["order"];
 	$h_group = $_REQUEST["group"];
+	$h_shipping = $_REQUEST["shipp"];
+	$ship = html_entity_decode($h_shipping);
 	$order = html_entity_decode($h_order);
 	$group = html_entity_decode($h_group);
+	$d_ship = json_decode($ship, TRUE);
 	$d_order = json_decode($order, TRUE);
 	$d_group = json_decode($group, TRUE);
 	$grouping = array("key");
@@ -26,6 +30,7 @@
 	*/
 	
 	
-	print "Rabbits :" . $d_order["Rabbits"] . "\t\t\t\t" . $grouping[1] . "\nLarge Easter Baskets :" . $d_order["LBaskets"] . "\t" . $grouping[2] . "\nSmall Easter Baskets :" . $d_order["SBaskets"] . "\t" . $grouping[3] . "\nEaster Eggs :" . $d_order["Eggs"] . "\t\t\t" . $grouping[4] . "\n";
-
+	//print "Rabbits :" . $d_order["Rabbits"] . "\t\t\t\t" . $grouping[1] . "\nLarge Easter Baskets :" . $d_order["LBaskets"] . "\t" . $grouping[2] . "\nSmall Easter Baskets :" . $d_order["SBaskets"] . "\t" . $grouping[3] . "\nEaster Eggs :" . $d_order["Eggs"] . "\t\t\t" . $grouping[4] . "\n";
+	print var_dump($d_ship);
+	//"Your desired delivery date is : " . $d_ship["Days"] . " days and " . $d_ship["Minutes"] . " minutes\n";
 ?>
