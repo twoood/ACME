bd4d57c1406decb1980cb8ceb7dbeab7e6d51c7b
twoood <31707618+twoood@users.noreply.github.com>
2018-09-06 13:35:57 -0500

    Update acme_server.php

--- acme_server.php
+++ acme_server.php
@@ -1,22 +1,31 @@
 <?php
-	// $q_rabbit = $_POST['Rabbits'];
-	// $q_lbasket = $_POST['LBaskets'];
-	// $q_sbasket = $_POST['SBaskets'];
 	$h_order = $_REQUEST["order"];
 	$h_group = $_REQUEST["group"];
 	$order = html_entity_decode($h_order);
 	$group = html_entity_decode($h_group);
 	$d_order = json_decode($order, TRUE);
 	$d_group = json_decode($group, TRUE);
-
-	foreach ($d_group as &$value) {
-		if ($value != "1")
-			$value = "0";
-	}
+	$grouping = array("key");
 	
-	foreach ($d_group as $value) {
+	
+	
+	foreach ($d_group as &$value) {
+		if ($value != "1") {
+			$value = "0";
+			array_push($grouping, "Not Grouped");
+		}
+		else {
+			array_push($grouping, "Grouped");
+		}
+	}
+	/*
+	foreach ($d_group as &$value) {
 		print $value;
 	}
+	print "\n";
+	*/
 	
-	print $d_group["Eggs"];
+	
+	print "Rabbits :" . $d_order["Rabbits"] . "\t\t\t\t" . $grouping[1] . "\nLarge Easter Baskets :" . $d_order["LBaskets"] . "\t" . $grouping[2] . "\nSmall Easter Baskets :" . $d_order["SBaskets"] . "\t" . $grouping[3] . "\nEaster Eggs :" . $d_order["Eggs"] . "\t\t\t" . $grouping[4] . "\n";
+
 ?>
