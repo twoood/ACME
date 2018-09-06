f799451b177fdae7b1e7912f741adc00e0683092
twoood <31707618+twoood@users.noreply.github.com>
2018-09-06 13:01:10 -0500

    Update acme_server.php

--- acme_server.php
+++ acme_server.php
@@ -3,12 +3,20 @@
 	// $q_lbasket = $_POST['LBaskets'];
 	// $q_sbasket = $_POST['SBaskets'];
 	$h_order = $_REQUEST["order"];
-	// $group = $_REQUEST["group"];
+	$h_group = $_REQUEST["group"];
 	$order = html_entity_decode($h_order);
+	$group = html_entity_decode($h_group);
 	$d_order = json_decode($order, TRUE);
-	// $d_group = json_decode($group, true);
+	$d_group = json_decode($group, TRUE);
 
-
-
-	print $d_order["Rabbits"];
+	foreach ($d_group as &$value) {
+		if ($value != "1")
+			$value = "0";
+	}
+	
+	foreach ($d_group as $value) {
+		print $value;
+	}
+	
+	print $d_group["Eggs"];
 ?>
