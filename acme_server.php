f56df130d6829c17c199862cba78086ba09d9e5b
Tom Wood <tpw0007@uah.edu>
2018-09-06 11:59:22 -0500

    Updated the AJAX functionality, still struggling with the PHP

--- acme_server.php
+++ acme_server.php
@@ -1,3 +1,13 @@
 <?php
-echo 'Thank you for your order of:'. $_POST['Rabbits']. ' Rabbits ' .$_POST['LBaskets']. ' Large Easter Baskets ' .$_POST['SBaskets']. ' Small Easter Baskets ' .$_POST['Eggs'] . ' Easter Eggs.';
+	// $q_rabbit = $_POST['Rabbits'];
+	// $q_lbasket = $_POST['LBaskets'];
+	// $q_sbasket = $_POST['SBaskets'];
+	$order = $_REQUEST["order"];
+	// $group = $_REQUEST["group"];
+	$d_order = json_decode($order, true);
+	// $d_group = json_decode($group, true);
+
+
+
+	print "This changes $d_order->{'Rabbits'}";
 ?>
